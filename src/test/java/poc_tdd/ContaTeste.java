f865b1a340e832b0ed0fd0f2800ac7edc12d5175
abizetto <abizetto@ciandt.com>
2018-08-06 16:14:52 -0300

    too commit add java class

---
+++ src/test/java/poc_tdd/ContaTeste.java
@@ -0,0 +1,12 @@
+package poc_tdd;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath: caracteristicas", tags = "@ContaTeste", glue = "cucumber.teste.passos", monochrome = true, dryRun = false)
+public class ContaTeste {
+
+}
